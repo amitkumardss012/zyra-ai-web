d16fb9d7f8738acbf6e4c635a6406c130e92eb16
amitkumardss012 <amitkumaryadav62029@gmail.com>
2026-04-17 16:18:43 +0530

    added auth secret

--- auth.ts
+++ auth.ts
@@ -8,4 +8,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET || "amit-kumar-yadav",
 });
+ 
